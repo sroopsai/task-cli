ac2b4b8aad50a61211685cf00db7a1ad7dc55340
roopsai <roopsai@outlook.in>
2024-12-30 16:15:12 +0530

    Add All Operations

--- src/main/java/in/roopsai/App.java
+++ src/main/java/in/roopsai/App.java
@@ -1,9 +1,10 @@
 package in.roopsai;
 
+import java.util.UUID;
+
 public class App {
 
     public static void main(String[] args) {
-
 
         start:
         while (true) {
@@ -15,12 +16,36 @@ public class App {
                 switch (command) {
                     case "add" -> {
                         if (!argument.isEmpty()) {
-                            Task.create(argument);
+                            Task.add(argument);
                         } else {
                             System.out.println("Please provide a description for the task.");
                         }
                     }
-                    case "list" -> Task.readAllTasks();
+                    case "list" -> {
+                        if (argument.isEmpty()) {
+                            Task.readAllTasks();
+                        } else {
+                            Status status = Status.valueOf(argument.toUpperCase());
+                            Task.readTasksBasedOnStatus(status);
+                        }
+
+                    }
+                    case "mark-in-progress" -> {
+                        if (argument.isEmpty()) {
+                            System.out.println("Please provide the ID of the task to mark as in progress.");
+                        } else {
+                            Task.markAsStatus(UUID.fromString(argument), Status.IN_PROGRESS);
+                        }
+                    }
+
+                    case "mark-done" -> {
+                        if (argument.isEmpty()) {
+                            System.out.println("Please provide the ID of the task to mark as done.");
+                        } else {
+                            Task.markAsStatus(UUID.fromString(argument), Status.COMPLETED);
+                        }
+                    }
+
                     case "exit" -> {
                         break start;
                     }
--- src/main/java/in/roopsai/Task.java
+++ src/main/java/in/roopsai/Task.java
@@ -8,52 +8,46 @@ import lombok.Builder;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 
 
 @Builder
-public record Task(int id, String description, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
-
-    private static final AtomicInteger idCounter = new AtomicInteger(0);
+public record Task(UUID id, String description, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
 
     private static final String FILE_NAME = "tasks.json";
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static Map<UUID, Task> tasks = new HashMap<>();
 
     static {
         objectMapper.registerModule(new JavaTimeModule());
     }
 
-    private static List<Task> tasks = new ArrayList<>();
-
     static {
         File taskFile = new File(FILE_NAME);
-        try {
-            if (taskFile.exists()) {
+        if (taskFile.exists()) {
+            taskFile.delete();
+            try {
+                taskFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Unable to create file: " + e.getMessage());
+            }
+
+            try {
                 tasks = objectMapper.readValue(taskFile, new TypeReference<>() {
                 });
+            } catch (IOException e) {
+                System.out.println("Unable to read tasks from file: " + e.getMessage());
+
             }
-        } catch (IOException e) {
-            System.out.println("Error reading tasks from file: " + e.getMessage());
         }
     }
 
 
-
-
-    public static void create(String description) {
-        Task task = Task
-                .builder()
-                .id(idCounter.incrementAndGet())
-                .description(description)
-                .status(Status.TODO)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
+    public static void add(String description) {
+        Task task = Task.builder().id(UUID.randomUUID()).description(description).status(Status.TODO).createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
 
         // Add it to the
-        tasks.add(task);
+        tasks.put(task.id(), task);
         task.saveToFile();
     }
 
@@ -61,8 +55,9 @@ public record Task(int id, String description, Status status, LocalDateTime crea
         File taskFile = new File(FILE_NAME);
         try {
             if (taskFile.exists()) {
-                List<Task> tasks = objectMapper.readValue(taskFile, new TypeReference<>() {
+                Collection<Task> tasks = objectMapper.readValue(taskFile, new TypeReference<>() {
                 });
+
                 tasks.forEach(System.out::println);
 
             }
@@ -72,15 +67,32 @@ public record Task(int id, String description, Status status, LocalDateTime crea
 
     }
 
-    public static void clearAllTasks() {
-
+    public static void readTasksBasedOnStatus(Status status) {
+        File taskFile = new File(FILE_NAME);
+        try {
+            if (taskFile.exists()) {
+                List<Task> tasks = objectMapper.readValue(taskFile, new TypeReference<>() {
+                });
+                tasks.stream().filter(task -> task.status().equals(status)).forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading tasks from file: " + e.getMessage());
+        }
     }
 
+    public static void markAsStatus(UUID taskId, Status status) {
+        // check if task exists
+        tasks.keySet().stream().filter(task -> task.equals(taskId)).findFirst().ifPresentOrElse(task -> {
+            Task newTask = Task.builder().id(task).createdAt(tasks.get(task).createdAt()).updatedAt(LocalDateTime.now()).status(status).description(tasks.get(task).description()).build();
+            tasks.put(newTask.id(), newTask);
+            newTask.saveToFile();
+        }, () -> System.out.println("Task with id " + taskId + " not found"));
+    }
 
     private void saveToFile() {
         try {
 
-            objectMapper.writeValue(new File(FILE_NAME), tasks);
+            objectMapper.writeValue(new File(FILE_NAME), tasks.values());
         } catch (IOException e) {
             System.out.println("Error saving task to file: " + e.getMessage());
         }
